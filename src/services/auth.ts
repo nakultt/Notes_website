27902c409056aaa80c41663c6e5ab3ef926c2dd9
nakultt <nakulnuked@gmail.com>
2025-09-05 20:03:51 +0530

    Fix API URL to point to the production server

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL="http://localhost:5173"
+const API_URL="https://notes-backend-t6y3.onrender.com"
 
 const Logins = async (email:string , password: string) => {
   const response = await axios.post(
